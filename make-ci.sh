1e42b8d10c45a3822074f6abe9da2abb3f23e374
Michele Brambilla <michele.brambilla@psi.ch>
2016-09-28 11:53:04 +0200

    testing libzmq

--- make-ci.sh
+++ make-ci.sh
@@ -7,17 +7,19 @@ env | sort
 sudo apt-get update
 sudo apt-get install -y libhiredis-dev libev-dev libgtest-dev redis-server
 
-# Make gtest
-git clone https://github.com/google/googletest
-cd googletest
-mkdir build
-cd build
-cmake -DBUILD_GMOCK=OFF -DBUILD_GTEST=ON -DBUILD_SHARED_LIBS=ON ..
-time make -j2
-cd ../..
-sudo cp googletest/build/googletest/libg* /usr/local/lib/
-sudo cp -r googletest/googletest/include/gtest /usr/local/include
+echo `pkg-config --cflags libzmq`
 
+## Make gtest
+#git clone https://github.com/google/googletest
+#cd googletest
+#mkdir build
+#cd build
+#cmake -DBUILD_GMOCK=OFF -DBUILD_GTEST=ON -DBUILD_SHARED_LIBS=ON ..
+#time make -j2
+#cd ../..
+#sudo cp googletest/build/googletest/libg* /usr/local/lib/
+#sudo cp -r googletest/googletest/include/gtest /usr/local/include
+#
 # Make
-make test
+#make test
 
--- test.cc
+++ test.cc
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "gtest/gtest.h"
 
-int main() {
-  std::cout << "ok" << std::endl;
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
